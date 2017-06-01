3947b3e7c210b4976e20054913c27d5593673514
Michele Finotto <m@finotto.org>
2017-06-01 17:54:16 +0200

    GcPeak should be a class

--- lib/gc_peak.rb
+++ lib/gc_peak.rb
@@ -2,17 +2,19 @@ require 'gc_peak/version'
 require 'json'
 
 module GcPeak
-  def initialize(app)
-    @app = app
+  class GcPeak
+    def initialize(app)
+      @app = app
 
-    @path = ENV['GC_PEAK_PATH'] || '/utils/gc_peak'
-    end
+      @path = ENV['GC_PEAK_PATH'] || '/utils/gc_peak'
+      end
 
-  def call(env)
-    if env['PATH_INFO'] == @path
-      [200, { 'Content-Type' => 'application/json' }, JSON.dump(GC.stat)]
-    else
-      @app.call(env)
+    def call(env)
+      if env['PATH_INFO'] == @path
+        [200, { 'Content-Type' => 'application/json' }, JSON.dump(GC.stat)]
+      else
+        @app.call(env)
+      end
     end
   end
 end
